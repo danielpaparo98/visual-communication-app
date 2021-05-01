41cfb38325034a99001ded7e0bc0465949887229
danielpaparo98 <21976306@student.uwa.edu.au>
2021-05-01 11:52:30 +0800

    Feat: Added barebones image picker modal

--- js/app.js
+++ js/app.js
@@ -1,20 +1,56 @@
-Vue.component('comm-card', {
+Vue.component('img-modal', {
+    data: function () {
+        return {
+
+        }
+    },
+
     template: `
-    <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-    <h4 class="card-title text-center">Insert text here</h4>
-    <p class="card-text text-center">Subtitle</p>
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Pick an icon</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                top
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary">Change Image</button>
+            </div>
+            </div>
+        </div>
     </div>
-    </div>
-    
     `
-  })
-  
+})
+
+Vue.component('comm-card', {
+    data: function () {
+        return {
+            img_addr: 'https://via.placeholder.com/20',
+            img_alt: '',
+            heading: '',
+            subtitle: '',
+        }
+    },
+    template: `
+    <div class="card">
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">            <img data-toggle="modal" v-bind:src="img_addr" class="card-img-top" alt="img_alt">
+        </button>
+        <img-modal></img-modal>
+        <div class="card-body">
+        <h4 class="text-center d-print-only">{{ heading }}</h4>
+        <input v-model="heading" class="heading-input text-center d-print-none">
+        <p class="text-center d-print-only">{{ subtitle }}</p>
+        <input v-model="subtitle" class="text-center d-print-none">
+        </div>
+    </div>
+    `
+})
+
 
 var app = new Vue({
     el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  })
+})
