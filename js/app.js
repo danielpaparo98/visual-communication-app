ec05f802ab498a52a1502920c149ef262621a3b3
danielpaparo98 <21976306@student.uwa.edu.au>
2021-05-08 14:31:03 +0800

    Feat: Updating card now updates array

--- js/app.js
+++ js/app.js
@@ -1,40 +1,3 @@
-Vue.component('chart-card', {
-    props: ['modal_id', 'img_addr', 'img_alt', 'heading', 'subtitle', 'icons'],
-    data: function () {
-        return {
-        }
-    },
-    template: `
-    <div class="col" >
-    <div class="card">
-    <b-button v-b-modal=modal_id class="modal-button">
-    <img v-bind:src="img_addr" class="card-img-top" :alt=img_alt>
-    </b-button>
-    
-    <div class="card-body">
-    <h5 class="text-center d-print-only">{{ heading }}</h5>
-    <input v-model="heading" maxlength="12" class="heading-input text-center d-print-none">
-    <p class="text-center d-print-only">{{ subtitle }}</p>
-    <input v-model="subtitle" maxlength="19" class="text-center d-print-none">
-    </div>
-    
-    
-    <b-modal :id="modal_id" size="lg" title="Select a new icon" centered ok-only scrollable  no-stacking>
-    <div class="container">
-    <div class="row">
-    <div v-for="icon in icons" :key="icon.message" class="col-4 col-sm-2">
-    <b-form-radio v-model="img_addr"  name="icons" :value="icon.addr" >
-    <img :src="icon.addr" :alt=icon.alt>
-    </b-form-radio>
-    </div>
-    </div>
-    </div>
-    </b-modal>
-    </div>
-    </div>
-    `
-})
-
 Vue.component('chart', {
     data: function () {
         return {
@@ -61,9 +24,8 @@ Vue.component('chart', {
         }
         else {
             // If there is no stored chart, create a new one from scratch
-            var img = this.icons[Math.floor(Math.random() * this.icons.length)];
             for (i = 0; i < 20; i++) {
-                img = this.icons[Math.floor(Math.random() * this.icons.length)];
+                var img = this.icons[Math.floor(Math.random() * this.icons.length)];
                 console.log()
                 this.cards.push({
                     id: 'card-' + i,
@@ -78,10 +40,42 @@ Vue.component('chart', {
 
         console.log(this.cards);
     },
+    watch: {
+        cards: function() {
+            console.log("updated!");
+            localStorage.setItem('chartSave', JSON.stringify(this.cards));
+        }
+    },
     template: `
     <div class = "row gy-4 row-cols-2 row-cols-sm-4">
-    <div v-for="card in cards">
-    <chart-card :modal_id="card.id" :icons="icons" :img_addr="card.img_addr" :img_alt="card.img_alt" :heading="card.heading" :subtitle="card.subtitle"></chart-card>
+    <div v-for="card in this.cards">
+    <div class="col" >
+    <div class="card">
+    <b-button v-b-modal=card.id class="modal-button">
+    <img v-bind:src="card.img_addr" class="card-img-top" :alt=card.img_alt>
+    </b-button>
+    
+    <div class="card-body">
+    <h5 class="text-center d-print-only">{{ card.heading }}</h5>
+    <input v-model="card.heading" maxlength="12" class="heading-input text-center d-print-none">
+    <p class="text-center d-print-only">{{ card.subtitle }}</p>
+    <input v-model="card.subtitle" maxlength="19" class="text-center d-print-none">
+    </div>
+    
+    
+    <b-modal :id="card.id" size="lg" title="Select a new icon" centered ok-only scrollable  no-stacking>
+    <div class="container">
+    <div class="row">
+    <div v-for="icon in icons" :key="icon.message" class="col-4 col-sm-2">
+    <b-form-radio v-model="card.img_addr"  name="icons" :value="icon.addr" >
+    <img :src="icon.addr" :alt=icon.alt>
+    </b-form-radio>
+    </div>
+    </div>
+    </div>
+    </b-modal>
+    </div>
+    </div>
     </div>
     </div>
     `
