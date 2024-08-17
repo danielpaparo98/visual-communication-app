7c2f7ca48e84875522d1abfc2ad74d5f849fa2b5
Daniel Paparo <21976306@student.uwa.edu.au>
2024-08-17 19:27:11 +0800

    ✨ add app framework

---
+++ src/components/AppRibbon.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="max-w-screen-xl mx-auto px-4 md:px-8">
+        <div class="items-start justify-between py-4 border-b md:flex">
+            <div>
+                <h3 class="text-gray-800 text-2xl font-bold">
+                    Chart creator
+                </h3>
+            </div>
+            <div class="items-center gap-x-3 mt-6 md:mt-0 sm:flex">
+                <a href="javascript:void(0)"
+                    class="block px-4 py-2 text-center text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm">
+                    Print
+                </a>
+                <a href="javascript:void(0)"
+                    class="block px-4 py-2 mt-3 text-center text-gray-700 duration-150 font-medium rounded-lg border hover:bg-gray-50 active:bg-gray-100 sm:mt-0 md:text-sm">
+                    New
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -1,15 +1,46 @@
-<template>
-  <div class="about">
-    <h1>This is the app page.</h1>
-  </div>
-</template>
+<script setup>
+import AppRibbon from '@/components/AppRibbon.vue';
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+</script>
+
+<template>
+  <AppRibbon />
+
+  <section class="max-w-screen-xl mx-auto px-4 md:px-8 my-10">
+
+    <div class="mx-auto mb-10 ">
+      <label class="block text-md font-medium text-gray-700"> Title </label>
+
+      <input placeholder="My chart title"
+        class="mt-1 w-124 rounded-md border-gray-400 shadow-sm text-4xl text-center" />
+    </div>
+
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-4 lg:gap-4">
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+      <div class="h-32 rounded-lg bg-gray-200"></div>
+    </div>
+  </section>
+
+</template>
