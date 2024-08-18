e269d99507faad8c2a47e074750ed1650d6a398a
Daniel Paparo <21976306@student.uwa.edu.au>
2024-08-18 10:45:43 +0800

    ✨ add branding

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
-  <footer class="text-gray-500 bg-white px-4 py-5">
+  <footer class="text-gray-500 bg-white px-4 py-5 border-t">
     <div class="max-w-lg sm:mx-auto sm:text-center">
-      <img src="https://www.floatui.com/logo.svg" class="w-32 sm:mx-auto" />
-      <p class="leading-relaxed mt-2 text-[15px]">
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-        galley of type and scrambled it to make a type specimen book.
+      <img src="/public/logo.svg" class="w-20 sm:mx-auto rounded-2xl" />
+      <p class=" leading-relaxed mt-2 text-[15px]">
+        An open source and free project built to help the community create easy and intuitive visual communication
+        charts.
       </p>
     </div>
     <ul class="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
