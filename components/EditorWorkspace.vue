7fc9e0031fb04003e2bd0c23db0bd5d76bf940f9
danielpaparo98 <paparodaniel98@gmail.com>
2026-03-28 21:38:58 +0800

    🐛 Fix: props.styleSettings is undefined error

--- components/EditorWorkspace.vue
+++ components/EditorWorkspace.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="editor-workspace flex-1 flex overflow-hidden relative">
     <!-- Side Panel (desktop) -->
-    <SidePanel 
+    <SidePanel
       v-if="shouldShowSidePanel"
       :is-open="editorLayout.state.sidePanelOpen"
       :active-section="editorLayout.state.activeSection"
@@ -11,7 +11,22 @@
     
     <!-- Canvas Area -->
     <div class="canvas-area flex-1 overflow-auto bg-gray-100 relative">
-      <EditorCanvas />
+      <EditorCanvas
+        :cards="chartStore.cards"
+        :canvas-settings="chartStore.canvasSettings"
+        :style-settings="chartStore.styleSettings"
+        :zoom="chartStore.zoom"
+        :is-preview-mode="chartStore.isPreviewMode"
+        :selected-card-id="chartStore.selectedCardId"
+        :title="chartStore.title"
+        :custom-icons="chartStore.customIcons"
+        @select-card="chartStore.selectCard"
+        @update-card="handleUpdateCard"
+        @delete-card="chartStore.removeCard"
+        @duplicate-card="chartStore.duplicateCard"
+        @reorder-cards="chartStore.reorderCards"
+        @add-card="chartStore.addCard"
+      />
     </div>
     
     <!-- Floating Controls -->
@@ -24,13 +39,37 @@ import SidePanel from './SidePanel.vue'
 import EditorCanvas from './EditorCanvas.vue'
 import FloatingControls from './FloatingControls.vue'
 import { useEditorLayoutStore } from '~/stores/editorLayout'
+import { useChartStore } from '~/stores/chart'
 
 const editorLayout = useEditorLayoutStore()
+const chartStore = useChartStore()
 
 // Computed
 const shouldShowSidePanel = computed(() => {
   return editorLayout.state.sidePanelOpen && editorLayout.state.viewportSize === 'desktop'
 })
+
+// Event handlers
+function handleUpdateCard(cardId: string, updates: any) {
+  const card = chartStore.cards.find(c => c.id === cardId)
+  if (card) {
+    if (updates.heading !== undefined) {
+      chartStore.updateCardHeading(cardId, updates.heading)
+    }
+    if (updates.subtitle !== undefined) {
+      chartStore.updateCardSubtitle(cardId, updates.subtitle)
+    }
+    if (updates.iconId !== undefined) {
+      chartStore.updateCardIcon(cardId, updates.iconId)
+    }
+    if (updates.customIconId !== undefined) {
+      chartStore.updateCardCustomIcon(cardId, updates.customIconId)
+    }
+    if (updates.textFormatting !== undefined) {
+      chartStore.updateCardTextFormatting(cardId, updates.textFormatting)
+    }
+  }
+}
 </script>
 
 <style scoped>
